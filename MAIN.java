a649eb975fcd745e100990bcddacb6aba52c2e6f
Rubenm24 <r.matton@student.maastrichtuniversity.nl>
2021-04-18 16:40:09 +0200

    Milestone 1

--- MAIN.java
+++ MAIN.java
@@ -1,6 +1,6 @@
 public class MAIN{
 	public static void main(String[]args){
-		System.out.println("WORK IN PRGRESS...");
-		System.out.println("WORK IN PRGRESS...2");
+		System.out.println("WORK IN PRGRESS....");
+		System.out.println("MILESTONE 1");
 	}
 }
